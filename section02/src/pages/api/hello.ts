e3e21480e1aae5f55a50b6dd8d24c5db19c76678
pokitong <yujusul@gmail.com>
2024-09-21 14:57:08 +0900

    섹션2 챕터5 API Routes 강의 수강 완료

--- section02/src/pages/api/hello.ts
+++ section02/src/pages/api/hello.ts
@@ -8,6 +8,11 @@ type Data = {
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
+
+  // request 와 response 를 매개변수로 받음
 ) {
   res.status(200).json({ name: "John Doe" });
 }
+
+// pages 내에 api 폴더를 만들고 해당 폴더 내에 새로운 파일들을 배치해주면
+// 해당 파일들은 API 라우츠로써 API 응답을 정의하는 파일이 된다.
