5f8c3f3baa13a364f30eb82cda6d794865196272
pokitong <yujusul@gmail.com>
2024-09-22 03:26:24 +0900

    섹션2 챕터14 SSG 2. 정적 경로에 적용하기 강의 수강 완료

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -7,33 +7,17 @@ import SearchableLayout from "@/components/searchable-layout";
 import {ReactNode} from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import {InferGetServerSidePropsType} from "next";
+import {InferGetServerSidePropsType, InferGetStaticPropsType} from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-randomBooks";
 
 // @ 경로 : src 폴더를 가리키는 경로
 
-export const getServerSideProps = async () => {
-    // getServerSideProps 라는 약속된 이름의 함수를 만들어서 export 로 내보내주면 해당 페이지는 SSR 방식으로 사전렌더링 된다.
-    // 클라이언트에서 페이지를 요청해서 넥스트가 사전 렌더링을 실행할 때, 해당 페이지 컴포넌트보다 먼저 실행되어서
-    // 컴포넌트에 필요한 데이터를 또 다른 백엔드 서버로부터 불러오는 등의 기능을 하는 함수
-
-    // -> 사전 렌더링을 하는 과정에서 단 한 번만 실행되기 때문에 오직 서버 측에서만 실행되는 함수라는 점 주의
-
-    // console.log("서버사이드 프롭스예요");
-    // -> 콘솔로 찍더라도 브라우저 콘솔에 나오지 않고, 실행중인 넥스트 서버의 로그가 나오고 있는 터미널에만 출력
-    // window.location; 자바스크립트의 window : 브라우저
-    // -> 클라이언트에서만 접근 가능한 window 같은 함수를 실행하려고 하면 브라우저를 읽을 수 없기 때문에 해당 값이
-    // undefined 가 되며, undefined 에서 location 을 호출하려고 할 시 오류 발생
-
-    /**
-     * 1. 클라이언트에서 페이지 요청
-     * 2. getServersideProps 함수 실행 (필요한 데이터 사전 페치 등의 동작 수행)
-     * 3. 컴포넌트 실행
-     */
-
-/*    const allBooks = await fetchBooks();
-    const recoBooks = await fetchRandomBooks();*/
+export const getStaticProps = async () => {
+    console.log("인덱스 페이지");
+    // getStaticProps : SSG 방식으로 렌더링하게 해주는 함수
+    // -> 개발 모드에서는 SSR 이든, SSG 든 개발자의 편의를 위해서 호출을 받을때마다 컴포넌트들을 새롭게 렌더링 해준다.
+    // SSG 방식이 디폴트
 
     const [allBooks, recoBooks] = await Promise.all([
         fetchBooks(),
@@ -50,18 +34,11 @@ export const getServerSideProps = async () => {
     // ServerSideProps 의 return 값은 반드시 props 라는 객체 프로퍼티를 포함하는 단 하나의 객체여야만 한다.
 };
 
-export default function Home({allBooks, recoBooks} : InferGetServerSidePropsType<typeof getServerSideProps>) {
-    // InferGetServerSidePropsType -> GetServerSideProps 함수에서 반환되는 값을 자동으로 타입 추론해주는 타입 객체
+export default function Home({allBooks, recoBooks}: InferGetStaticPropsType<typeof getStaticProps>) {
 
-    // Home 컴포넌트 또한 서버에서 한 번 실행된 후 브라우저에서 한 번 더 실행된다.
-    // -> 브라우저로부터 접속 요청을 받았을 때 사전 렌더링을 위해 서버측에서 첫번째로 실행
-    // -> 브라우저에서 자바스크립트 번들 형태로 전달되어서 하이드레이션 과정이 진행 될 때 두번째로 실행
-
-    // !!! 그렇기 때문에 컴포넌트 내부에서 바로 브라우저에서만 실행 가능한 함수를 호출 시 오류 발생 !!!
-
-/*    useEffect(() => {
-        console.log(window);
-    }, []);*/
+    /*    useEffect(() => {
+            console.log(window);
+        }, []);*/
     // -> 브라우저에서만 실행 가능한 함수를 호출하고 싶을 때는 useEffect 사용
 
     return (
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,13 +1,43 @@
-import books from "@/mock/books.json";
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useState} from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
-import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
+import {GetServerSidePropsContext, GetStaticPropsContext, InferGetServerSidePropsType} from "next";
 import fetchBooks from "@/lib/fetch-books";
+import {useRouter} from "next/router";
+import {BookData} from "@/types";
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+/*export const getStaticProps = async (context: GetStaticPropsContext) => {
     // context : 현재 브라우저로부터 받은 요청에 대한 모든 정보가 다 포함되어 있음
-    const q = context.query.q;
+
+    // getServerSideProps 라는 약속된 이름의 함수를 만들어서 export 로 내보내주면 해당 페이지는 SSR 방식으로 사전렌더링 된다.
+    // 클라이언트에서 페이지를 요청해서 넥스트가 사전 렌더링을 실행할 때, 해당 페이지 컴포넌트보다 먼저 실행되어서
+    // 컴포넌트에 필요한 데이터를 또 다른 백엔드 서버로부터 불러오는 등의 기능을 하는 함수
+
+    // -> 사전 렌더링을 하는 과정에서 단 한 번만 실행되기 때문에 오직 서버 측에서만 실행되는 함수라는 점 주의
+
+    // console.log("서버사이드 프롭스예요");
+    // -> 콘솔로 찍더라도 브라우저 콘솔에 나오지 않고, 실행중인 넥스트 서버의 로그가 나오고 있는 터미널에만 출력
+    // window.location; 자바스크립트의 window : 브라우저
+    // -> 클라이언트에서만 접근 가능한 window 같은 함수를 실행하려고 하면 브라우저를 읽을 수 없기 때문에 해당 값이
+    // undefined 가 되며, undefined 에서 location 을 호출하려고 할 시 오류 발생
+
+    /!**
+     * 1. 클라이언트에서 페이지 요청
+     * 2. getServersideProps 함수 실행 (필요한 데이터 사전 페치 등의 동작 수행)
+     * 3. 컴포넌트 실행
+     *!/
+
+    /!*    const allBooks = await fetchBooks();
+        const recoBooks = await fetchRandomBooks();*!/
+
+/!*    const q = context.query.q;*!/
+    // GetStaticProps 함수에게 전달되는 context 파라미터에는 쿼리스트링을 포함하고 있는  query 프로퍼티가 존재하지 않는다.
+    // -> 빌드 당시 단 한 번만 실행되므로 빌드 타임에 query 를 알아낼 방법이 없다.
+    // -> 쿼리스트링을 꺼내올 방법이 없기 때문에 서치같은 페이지는 SSG 방식으로 렌더할 수 없다.
+    // -> 꼭 하고싶다면 현재의 쿼리스트링을 꺼내와서 해당 갑승로 결과 데이터를 불러오는 과정을 리액트와 같이 사전 렌더링
+    // 이후에 진행해주어야 한다.
+
+
     const books = await fetchBooks(q as string);
 
     return {
@@ -15,9 +45,33 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
             books,
         },
     };
-};
+};*/
 
-export default function Page({books}:InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page() {
+    // InferGetServerSidePropsType -> GetServerSideProps 함수에서 반환되는 값을 자동으로 타입 추론해주는 타입 객체
+
+    // Home 컴포넌트 또한 서버에서 한 번 실행된 후 브라우저에서 한 번 더 실행된다.
+    // -> 브라우저로부터 접속 요청을 받았을 때 사전 렌더링을 위해 서버측에서 첫번째로 실행
+    // -> 브라우저에서 자바스크립트 번들 형태로 전달되어서 하이드레이션 과정이 진행 될 때 두번째로 실행
+
+    // !!! 그렇기 때문에 컴포넌트 내부에서 바로 브라우저에서만 실행 가능한 함수를 호출 시 오류 발생 !!!
+    const [books, setBooks] = useState<BookData[]>([]);
+
+    const router = useRouter();
+
+    const q = router.query.q;
+
+    const fetchSearchResult = async() => {
+        const data = await fetchBooks(q as string);
+        setBooks(data);
+    }
+
+    useEffect(() => {
+        if (q) {
+            // 검색 결과를 불러오는 로직
+            fetchSearchResult();
+        }
+    }, [q]);
 
 /*
     const router = useRouter()
