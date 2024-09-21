b7d107a6103af0ade77eaa1826f8b9b16a2cd463
pokitong <yujusul@gmail.com>
2024-09-22 03:39:27 +0900

    섹션2 챕터15 SSG 3.동적 경로에 적용하기 강의 수강 완료

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,19 +1,12 @@
 import style from "./[id].module.css";
-import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
-import fetchBooks from "@/lib/fetch-books";
+import {
+    GetStaticPropsContext,
+    InferGetStaticPropsType
+} from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
 
-const mockData = {
-    id: 1,
-    title: "한 입 크기로 잘라 먹는 리액트",
-    subTitle: "자바스크립트 기초부터 애플리케이션 배포까지",
-    description: "자바스크립트 기초부터 애플리케이션 배포까지\n처음 시작하기 딱 좋은 리액트 입문서\n\n이 책은 웹 개발에서 가장 많이 사용하는 프레임워크인 리액트 사용 방법을 소개합니다. 인프런, 유데미에서 5000여 명이 수강한 베스트 강좌를 책으로 엮었습니다. 프런트엔드 개발을 희망하는 사람들을 위해 리액트의 기본을 익히고 다양한 앱을 구현하는 데 부족함이 없도록 만들었습니다. \n\n자바스크립트 기초 지식이 부족해 리액트 공부를 망설이는 분, 프런트엔드 개발을 희망하는 취준생으로 리액트가 처음인 분, 퍼블리셔나 백엔드에서 프런트엔드로 직군 전환을 꾀하거나 업무상 리액트가 필요한 분, 뷰, 스벨트 등 다른 프레임워크를 쓰고 있는데, 실용적인 리액트를 배우고 싶은 분, 신입 개발자이지만 자바스크립트나 리액트 기초가 부족한 분에게 유용할 것입니다.",
-    author: "이정환",
-    publisher: "프로그래밍인사이트",
-    coverImgUrl: "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg"
-};
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+/*export const getServerSideProps = async (context: GetServerSidePropsContext) => {
     const id = context.params!.id;
     // [id] 페이지는 무조건 url 파라미터가 있어야만 접근할 수 있는 페이지이기 때문에 ! 단언을 사용해도 안전하다.
 
@@ -23,9 +16,39 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
     return {
         props: {book}
     }
+};*/
+
+export const getStaticPaths = () => {
+    // 동적 경로 페이지의 경우, 가능한 페이지 경로들 (id의 개수 만큼) 을 먼저 getStaticPaths 로 정의해주고, 해당
+    // 경로들에 맞춰서 getStaticProps 를 그 수만큼 호출해서 해당 페이지들을 전부 사전에 렌더링 해주어야 한다.
+
+    return {
+        paths : [
+            {params: {id: "1"}},
+            {params: {id: "2"}},
+            {params: {id: "3"}},
+        ],
+        fallback: false,
+        // fallback : 대체, 대비책, 보험
+        // path 값에 설정하지 않은 url 로 요청하게 되면 반환할 대비책
+        // 1. false : 없는 페이지로 취급해서 Not found 반환
+
+    }
+    // 리턴 객체 내부에 paths 라는 이름으로 가능한 params 의 배열을 반환해주어야 한다.
+    // paths 배열 내부에는 가능한 param 들의 객체들을 정의해주면 된다.
+    // !! url 파라미터의 값은 반드시 문자열로 설정해주어야 Next 프레임워크가 제대로 읽을 수 있다.
 };
 
-export default function Page({book}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+    const id = context.params!.id;
+    const book = await fetchOneBook(Number(id));
+
+    return {
+        props: {book}
+    }
+};
+
+export default function Page({book}: InferGetStaticPropsType<typeof getStaticProps>) {
     if (!book) return "문제가 발생했습니다 다시 시도하세요";
 
     const {id, title, subTitle, description, author, publisher, coverImgUrl} = book;
