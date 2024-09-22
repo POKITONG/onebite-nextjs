1e7399ebc2da41ce083bdbd82d41ceb1d8cfe5d3
pokitong <yujusul@gmail.com>
2024-09-22 20:45:51 +0900

    섹션3 챕터3 레이아웃 설정하기 강의 수강 완료

---
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from "react";
+
+export default function Layout({children}: { children: ReactNode}) {
+    return <div>
+        <div>임시 서치바</div>
+        {children}
+    </div>;
+};
+
+// 라우트 그룹 : (name) 형태로 된 디렉토리로, 경로에 어더한 영향도 미치지 않는 폴더
+// -> 동시에 각기 다른 경로를 갖는 페이지 파일들을 하나의 폴더 안에 묶어둘 수 있다.
+// -> 경로상에 영향을 미치지 않으면서 레이아웃 파일을 만들 수 있다.
